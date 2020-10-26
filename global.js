a50a5d396e37f0073bb5fa79563da6fec609caed
Nico Bachner <yo@nicob.dev>
2020-10-26 10:43:10 +0100

    api consumption test 2

--- global.js
+++ global.js
@@ -1,12 +1,16 @@
-var membersSection = document.getElementById("members-content");
+async function addMembers() {
+    var membersSection = document.getElementById("members-content");
 
-// fetch data
-let response = await fetch('https://api.sos-school.org/members');
-let members = await response.json();
+    // fetch data
+    let response = await fetch('https://api.sos-school.org/members');
+    let members = await response.json();
 
-// populate members section with fetched data
-for (var i = 0; i < members.length; i++) {
-    var member = document.createElement("p");
-    member.innerHTML = members[i].firstName + ' ' + members[i].lastName;
-    membersSection.appendChild(member);
+    // populate members section with fetched data
+    for (var i = 0; i < members.length; i++) {
+        var member = document.createElement("p");
+        member.innerHTML = members[i].firstName + ' ' + members[i].lastName;
+        membersSection.appendChild(member);
+    }
 }
+
+addMembers();
