d069679b9673f342209afd5f30a6f08cbbe93832
Nico Bachner <yo@nicob.dev>
2020-10-26 19:18:41 +0100

    make members list a list

--- global.js
+++ global.js
@@ -33,7 +33,7 @@ async function addMembers() {
 
     // populate members section with fetched data
     for (var i = 0; i < members.length; i++) {
-        var member = document.createElement("p");
+        var member = document.createElement("li");
         member.innerHTML = members[i].firstName + ' ' + members[i].lastName;
         membersSection.appendChild(member);
     }
