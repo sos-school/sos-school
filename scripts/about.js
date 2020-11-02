1ebafc057e142740274f5ff565cac9141d1b37b7
Nico Bachner <yo@nicob.dev>
2020-11-02 12:05:48 +0100

    fixed fetch file format for about section

--- scripts/about.js
+++ scripts/about.js
@@ -3,7 +3,9 @@ export default async function addAbout() {
 
     // fetch data
     let response = await fetch(`https://raw.githubusercontent.com/sos-school/website/master/en/README.md`);
-    let about = await response.json();
+    let about = await response.body;
+    console.log(response)
+    console.log(response.type)
 
     // inject data into about section
     document.getElementById('about').innerHTML = marked(about)
