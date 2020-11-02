924d05daac27cc4d127fc194442b670d1846a7ce
Nico Bachner <yo@nicob.dev>
2020-11-02 13:15:03 +0100

    added padding to nav links

--- scripts/about.js
+++ scripts/about.js
@@ -2,7 +2,7 @@ export default async function addAbout(lang) {
     // fetch data
     let response = await fetch(`../${lang}/README.md`);
     let about = await response.text;
-    console.log(response);
+    console.log(about);
 
     // inject data into about section
     document.getElementById("about").innerHTML = marked(about);
