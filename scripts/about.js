2ef68bb467e19b9108dddc4f6530eff12e2041c7
Nico Bachner <yo@nicob.dev>
2020-11-02 13:02:21 +0100

    fixed some styles

--- scripts/about.js
+++ scripts/about.js
@@ -1,10 +1,4 @@
 export default async function addAbout(lang) {
-    // import Marked
-    markedImport = document.createElement('script');
-    markedImport.src ='https://cdn.jsdelivr.net/npm/marked/marked.min.js';
-
-    document.getElementsByTagName('head')[0].appendChild(markedImport);
-
     // fetch data
     let response = await fetch(`../${lang}/README.md`);
     let about = await response.text;
