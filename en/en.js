8f92503c31b49c39daf8415c24d1c686e54e33ff
Nico Bachner <yo@nicob.dev>
2020-11-02 13:45:32 +0100

    inlined es6 imports

--- en/en.js
+++
@@ -1,7 +0,0 @@
-import addLanguages from '../scripts/languages.js';
-import addMembers from '../scripts/members.js';
-
-window.onload = function addContent() {
-    addLanguages("en");
-    addMembers();
-}
--- fr/fr.js
+++
@@ -1,7 +0,0 @@
-import addLanguages from '../scripts/languages.js';
-import addMembers from '../scripts/members.js';
-
-window.onload = function addContent() {
-    addLanguages("fr");
-    addMembers();
-}
