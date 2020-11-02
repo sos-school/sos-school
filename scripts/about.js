d6391ebf75d52fd49dc6abf16c464ea0a44675db
Nico Bachner <yo@nicob.dev>
2020-11-02 12:51:31 +0100

    add marked import

--- scripts/about.js
+++ scripts/about.js
@@ -1,4 +1,10 @@
 export default async function addAbout(lang) {
+    // import Marked
+    markedImport = document.createElement('script');
+    markedImport.src ='https://cdn.jsdelivr.net/npm/marked/marked.min.js';
+
+    document.getElementsByTagName('head')[0].appendChild(markedImport);
+
     // fetch data
     let response = await fetch(`../${lang}/README.md`);
     let about = await response.text;
