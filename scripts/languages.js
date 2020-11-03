77cd1123940db5732ddf20617b5c736ce4a79615
Nico Bachner <yo@nicob.dev>
2020-11-03 11:38:07 +0100

    removed button from language links

--- scripts/languages.js
+++ scripts/languages.js
@@ -7,21 +7,20 @@ export default async function addLanguages(lang) {
 
     // inject data into language navigation
     for (var i = 0; i < languages.length; i++) {
+        // exclude current page
         if (lang != languages[i].abbreviation) {
+            // create link element
             var languageLink = document.createElement("a");
             languageLink.href = `https://www.sos-school.org/${languages[i].abbreviation}`;
-
+            languageButton.innerHTML = languages[i].flag + " " + languages[i].language;
+            
+            // fade-in animation
+            var fadeInDelay = i / 3;
             languageLink.style.animationName = "fadein";
             languageLink.style.animationDuration = `1s`;
-            
-            var fadeInDelay = i / 3;
             languageLink.style.animationDelay = `${fadeInDelay}s`;
             languageLink.style.animationFillMode = "both";
 
-            var languageButton = document.createElement("button");
-            languageButton.innerHTML = languages[i].flag + " " + languages[i].language;
-
-            languageLink.appendChild(languageButton);
             languageNavigation.appendChild(languageLink);
         }
     }
