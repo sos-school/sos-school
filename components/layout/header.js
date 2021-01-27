15583e10fc1b939b830bb3bcea4a31251e136edd
Nico Bachner <yo@nicob.dev>
2021-01-27 13:58:23 +0100

    justify text

--- components/layout/header.js
+++ components/layout/header.js
@@ -5,6 +5,16 @@ import { snippets, pages } from '../../content'
 import { languages } from '../../data'
 import i18n from '../../lib/i18n'
 
+function NavLink(props){
+    return (
+        <li className="flex-grow self-center">
+            <Link href={props.href} as={props.as}>
+                <a>{props.children}</a>
+            </Link>
+        </li>
+    )
+}
+
 export default function Header() {
     const router = useRouter();
     const t = i18n(router.locale, snippets);
@@ -18,33 +28,17 @@ export default function Header() {
     return (
         <nav className="px-4 md:px-8 lg:px-12 xl:px-16 py-4 flex justify-between">
             <Link href="/">
-                <Image 
-                    src="/logo.png" 
-                    width="48" 
+                <Image
+                    src="/logo.png"
+                    width="48"
                     height="48"
                 />
             </Link>
             <ul className="hidden sm:flex justify-center space-x-8">
-                <li className="flex-grow self-center">
-                    <Link href="/" as={p.home.id}>
-                        <a>{p.home.value}</a>
-                    </Link>
-                </li>
-                <li className="flex-grow self-center">
-                    <Link href="/about" as={p.about.id}>
-                        <a>{p.about.value}</a>
-                    </Link>
-                </li>
-                <li className="flex-grow self-center">
-                    <Link href="/team" as={p.team.id}>
-                        <a>{p.team.value}</a>
-                    </Link>
-                </li>
-                <li className="flex-grow self-center">
-                    <Link href="/contact" as={p.contact.id}>
-                        <a>{p.contact.value}</a>
-                    </Link>
-                </li>
+                <NavLink href="/" as={p.home.id}>{p.home.value}</NavLink>
+                <NavLink href="/about" as={p.about.id}>{p.about.value}</NavLink>
+                <NavLink href="/team" as={p.team.id}>{p.team.value}</NavLink>
+                <NavLink href="/contact" as={p.contact.id}>{p.contact.value}</NavLink>
             </ul>
             <select
                 onChange={changeLanguage}
